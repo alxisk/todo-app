4a2c9dc8c9e2daf02c1e762f2309cc3725d6abf2
alxisk <alexander.iskovskikh@gmail.com>
2017-04-22 23:35:50 +0300

    general setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   context: path.resolve(__dirname, './src'),
-  entry: 'index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
@@ -18,6 +19,18 @@ const config = {
         }
       }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Simple todo app'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 8080
   }
 }
 
